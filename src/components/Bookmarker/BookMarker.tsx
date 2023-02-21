b4b7372a71f6c4cce8843a50abdfb42249a63f17
H4Nhs <gksgytmd19@kakao.com>
2023-02-21 15:31:47 +0900

    chore: Bookmarker 일부 수정 및 폴더 구조 병합

--- src/components/Bookmarker/BookMarker.tsx
+++ src/components/Bookmarker/BookMarker.tsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useRecoilState } from "recoil";
 import { bookmarker } from "../../recoil/store";
+import styled from "styled-components";
 
 const BookMarker = () => {
   const [myBookMarker, setMyBookMarker] = useRecoilState(bookmarker);
@@ -52,17 +53,60 @@ const BookMarker = () => {
     <>
       <div>
         <div></div>
+        <input></input>
         <button>추가</button>
         <button>삭제</button>
       </div>
-      <div>
+      <ListContainer>
+        <ListhHead>
+          <CoinName>
+            <span>한글명</span>
+          </CoinName>
+          <CoinPrice>
+            <span>현재가</span>
+          </CoinPrice>
+          <CoinPrevPrice>
+            <span>전일대비</span>
+          </CoinPrevPrice>
+          <CoinValue>
+            <span>거래대금</span>
+          </CoinValue>
+        </ListhHead>
         {allCoinList.map((data: any) => {
-          // console.log(allCoinList);
-          return <div key={data.market}>{data.market}</div>;
+          console.log(data);
+          return (
+            <CoinBlock key={data.market}>
+              <span>{data.korean_name}</span>
+              <br />
+              <span>{data.market}</span>
+            </CoinBlock>
+          );
         })}
-      </div>
+      </ListContainer>
     </>
   );
 };
-
 export default BookMarker;
+
+//모든 코인들이 출력될 컨테이너
+const ListContainer = styled.div`
+  display: block;
+  margin: 0 auto;
+  width: 30rem;
+  height: 40rem;
+  border: 1px solid black;
+  overflow: scroll;
+`;
+//코인 리스트 상단
+const ListhHead = styled.div`
+  display: flex;
+`;
+const CoinName = styled.div``;
+const CoinPrice = styled.div``;
+const CoinPrevPrice = styled.div``;
+const CoinValue = styled.div``;
+
+// 리스트에 각 코인의 정보를 담음 (map으로 찍어냄)
+const CoinBlock = styled.div`
+  display: block;
+`;
