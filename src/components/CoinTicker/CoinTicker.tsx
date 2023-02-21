b1952627b7f207c5289b2dd0b2ca8f5ad4fb6694
shkiimaa <shkiimaa@gmail.com>
2023-02-21 15:36:30 +0900

    refactor: 코인시세조회 웹소켓 로직 수정

--- src/components/CoinTicker/CoinTicker.tsx
+++ src/components/CoinTicker/CoinTicker.tsx
@@ -1,10 +1,18 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback, useRef } from "react";
 
 // interface CoinList {
 //   KRW: { [key: string]: string }[];
 //   BTC: { [key: string]: string }[];
 // }
+
+type TCoin = {
+  code: string;
+};
+
+type TCoinList = {
+  [key: string]: TCoin;
+};
 
 const CoinTicker = () => {
   const [coinList, setCoinList] = useState<any>({
@@ -12,16 +20,15 @@ const CoinTicker = () => {
     BTC: []
   });
 
-  const [시세, set시세] = useState<any>({
-    KRW: []
-  });
+  const [allCoin, setAllCoin] = useState<TCoinList>({});
+  const ws = useRef<any>();
 
   const getCoinName = async () => {
     try {
       const config = { params: { isDeatils: true } };
       const response: any = await axios.get("https://api.upbit.com/v1/market/all", config);
-      // allCoin = response.data;
       const coinList = response.data;
+
       const KRWCoinName = coinList
         .filter((data: { [key: string]: string }) => data.market.includes("KRW-"))
         .map((data: any) => data);
@@ -33,86 +40,89 @@ const CoinTicker = () => {
         return { ...prevState, KRW: KRWCoinName, BTC: BTCCoinName };
       });
 
-      const responseKRW = coinList
-        .filter((data: { [key: string]: string }) => data.market.includes("KRW-"))
-        .map((data: any) => data.market);
-
-      const responseBTC = coinList
-        .filter((data: { [key: string]: string }) => data.market.includes("BTC-"))
-        .map((data: any) => data.market);
-
-      const ws = new WebSocket("wss://api.upbit.com/websocket/v1");
-      ws.onopen = () => {
-        ws.send(`[{"ticket":"test"},{"type":"ticker","codes": ["KRW-BTC"]}]`);
-      };
-
-      const allCoin: any[] = [];
-      ws.onmessage = async (e) => {
-        const { data } = e;
-        const text = await new Response(data).text();
-        const text2 = JSON.parse(text);
-        console.log(text2);
-        // allCoin.push(text2);
-        set시세((prevState: any) => {
-          return { KRW: [text2] };
-        });
-        // console.log(allCoin);
-      };
-
-      ws.onerror = (e) => {
-        console.log(e);
-      };
-
-      // let KRWCoin = "";
-      // let BTCCoin = "";
-      // for (const coin of coinList) {
-      //   if (!coin.market.indexOf("KRW")) {
-      //     console.log(coin.market.indexOf("KRW"));
-      //     KRWCoin += `${coin.market},`;
-      //   } else {
-      //     BTCCoin += `${coin.market},`;
-      //   }
-      // }
-
-      // await setCoinMarket(() => {
-      //   return { KRW: KRWCoin.substring(0, KRWCoin.length - 1), BTC: BTCCoin.substring(0, BTCCoin.length - 1) };
-      // });
-
-      // return KRWCoin.substring(0, KRWCoin.length - 1);
+      return coinList;
     } catch (error) {
       console.log(error);
     }
   };
 
-  console.log(시세);
-  // const getAllCoinTicker = async (coinMarket: any) => {
-  //   console.log(coinMarket);
-  //   try {
-  //     const config = {
-  //       params: { markets: coinMarket }
-  //     };
-  //     const response: any = await axios.get("https://api.upbit.com/v1/ticker", config);
-  //     console.log(response.data);
-  //     // for (let i = 0; i <= response.data.length; i++) {}
-  //   } catch (error) {
-  //     console.log(coinMarket);
-  //     console.log(error);
-  //   }
-  // };
+  const onWebSocket = () => {
+    const responseKRW = coinList.KRW.filter((data: { [key: string]: string }) => data.market.includes("KRW-")).map(
+      (data: any) => data.market
+    );
+    const responseBTC = coinList.BTC.filter((data: { [key: string]: string }) => data.market.includes("BTC-")).map(
+      (data: any) => data.market
+    );
+    ws.current = new WebSocket("wss://api.upbit.com/websocket/v1");
+
+    ws.current.onopen = () => {
+      // ws.send(`[{"ticket":"test"},{"type":"ticker","codes": ${JSON.stringify(responseKRW)}}]`);
+      ws.current.send(`[{"ticket":"test"},{"type":"ticker","codes": ['KRW-TON', 'KRW-CRE']}]`);
+    };
+
+    ws.current.onerror = (e: any) => {
+      console.log(e);
+    };
+  };
+
+  console.log(allCoin);
 
   useEffect(() => {
-    getCoinName(); /* .then((res) => {
-      getAllCoinTicker(res);
-    }); */
+    getCoinName().then(() => {
+      onWebSocket();
+    });
   }, []);
-  console.log(시세);
+
+  useEffect(() => {
+    if (!ws.current) return;
+    ws.current.onmessage = async (e: any) => {
+      const { data } = e;
+      const text = await new Response(data).text();
+      const parseText = JSON.parse(text);
+
+      // --------------------------------------
+
+      const allCoin = {
+        "KRW-CRE": {},
+        "KRW-TON": {},
+        "KRW-BTC": {}
+      };
+
+      setAllCoin((prev: any) => ({
+        ...prev,
+        [parseText.code]: parseText
+      }));
+
+      const allCoinArray = Object.values(allCoin); // [{code:"KRW-CRE", ...}, {code:"KRW-BTC", ...}]
+
+      // if (!allCoin.map((list: any) => list.code).includes(parseText.code)) {
+      //   setAllCoin((prevState: any) => {
+      //     // console.log(prevState.concat(parseText));
+      //     return prevState.concat(parseText);
+      //   });
+      // } else {
+      //   setAllCoin((prevState: any) => {
+      //     // console.log(prevState.fillter((list: any) => list.code !== parseText.code).concat(parseText));
+      //     return prevState
+      //       .filter((list: any) => {
+      //         console.log("hey", list.code, parseText.code);
+      //         return list.code !== parseText.code;
+      //       })
+      //       .concat(parseText);
+      //   });
+      // }
+
+      // --------------------------------------
+    };
+  }, [ws.current, allCoin]);
 
   return (
     <div>
-      {시세.KRW.map((data: any) => {
+      {JSON.stringify(Object.values(allCoin))}
+      {/* {시세.KRW.map((data: any) => {
         console.log(data);
         return <div key={data.code}>{data.trade_price}</div>;
-      })}
+      })} */}
     </div>
   );
 };
