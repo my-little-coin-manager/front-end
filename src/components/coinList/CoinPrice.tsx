734fae47e3f884d18bdb8db2683e4ec4e2f3e83d
H4Nhs <gksgytmd19@kakao.com>
2023-03-29 22:47:30 +0900

    docs: readme 작성

---
+++ src/components/coinList/CoinPrice.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import styled from "styled-components";
+
+interface PriceProps {
+  price: number;
+  change: string;
+}
+
+const CoinPrice = ({ price, change }: PriceProps) => {
+  return <TickerPrice change={change}>{price.toLocaleString("ko-KR")}</TickerPrice>;
+};
+
+export default CoinPrice;
+
+const TickerPrice = styled.p<{ change: string }>`
+  width: 25%;
+  font-size: 12px;
+  color: ${({ change }) => (change === "RISE" ? "#c84a31" : change === "FALL" ? "#1261c4" : "#333")};
+  text-align: right;
+  font-weight: 700;
+`;
