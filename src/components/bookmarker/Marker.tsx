8035b65175cf5b71c80bb504312eca23faf1c7e4
shkiimaa <shkiimaa@gmail.com>
2023-04-05 20:23:09 +0900

    refactor: 리액트쿼리 사용으로 리코일 비동기로직 제거

--- src/components/bookmarker/Marker.tsx
+++ src/components/bookmarker/Marker.tsx
@@ -1,33 +1,30 @@
 import React from "react";
-import { useRecoilState, useRecoilValue } from "recoil";
-import { userBookmark, coinSelect } from "../../recoil/atoms";
-import useGetBookmark from "hooks/bookmark/useGetBookmark";
-import instance from "service/API";
+import { useRecoilValue } from "recoil";
+import { coinSelect } from "../../recoil/atoms";
 import { ReactComponent as DisabledStar } from "../../asset/svg/star-disabled.svg";
 import { ReactComponent as AbledStar } from "../../asset/svg/star-abled.svg";
+import useGetBookmark from "hooks/bookmark/useGetBookmark";
 import usePutBookmark from "hooks/bookmark/usePutBookmark";
-import { useMutation } from "react-query";
 
-// interface NameBoxProps {
-//   select: string;
-// }
-// { select }: NameBoxProps
 const BookMarker = () => {
-  const { data } = useGetBookmark();
-  const { mutate } = usePutBookmark();
-  const selected = useRecoilValue<any>(coinSelect);
-  const [bookmarkInfo, setBookmarkInfo] = useRecoilState(userBookmark);
+  const { data: bookmark } = useGetBookmark();
+  const { mutate: putBookmark } = usePutBookmark();
+  const selected = useRecoilValue<string>(coinSelect);
+
+  console.log(bookmark);
 
   const changeStatus = () => {
-    mutate(data, selected);
+    if (!localStorage.getItem("accessToken")) {
+      alert("북마크 기능은 로그인 후 사용할 수 있습니다.");
+    } else {
+      putBookmark({ selected, bookmark });
+    }
   };
-
-  console.dir(mutate);
 
   return (
     <div onClick={changeStatus}>
-      {bookmarkInfo?.includes(selected) && <AbledStar />}
-      {!bookmarkInfo?.includes(selected) && <DisabledStar />}
+      {bookmark?.includes(selected) && <AbledStar />}
+      {!bookmark?.includes(selected) && <DisabledStar />}
     </div>
   );
 };
