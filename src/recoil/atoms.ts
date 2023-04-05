1f6fa7411fc2bef054f44535c21669aba870bc8b
shkiimaa <shkiimaa@gmail.com>
2023-04-05 20:22:05 +0900

    refactor: 비동기 리액트쿼리로 사용으로 bookmark 아톰 삭제

--- src/recoil/atoms.ts
+++ src/recoil/atoms.ts
@@ -31,12 +31,6 @@ export const liveCandleData = atom({
   default: {}
 });
 
-//북마크 정보
-export const userBookmark = atom<string[] | string>({
-  key: "userBookmark",
-  default: []
-});
-
 //전체코인 or 북마크 선택
 export const menuSelect = atom<string>({
   key: "menuSelect",
