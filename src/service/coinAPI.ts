081dfa5750bfb8705a2751bbb82a0c51c0cebcab
shkiimaa <shkiimaa@gmail.com>
2023-04-10 16:52:54 +0900

    chore: 필요없는 코드 삭제

--- src/service/coinAPI.ts
+++ src/service/coinAPI.ts
@@ -1,8 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 
 const instance: AxiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_COIN_API_URL,
-  headers: { Authorization: `Bearer ${localStorage.accessToken}` }
+  baseURL: process.env.REACT_APP_COIN_API_URL
 });
 
 instance.interceptors.request.use((config) => {
